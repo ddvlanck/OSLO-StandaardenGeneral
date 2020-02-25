d7e02cf8953b51bccf2e142a9563a16355dcf7ea
Dwight Van Lancker <dwight.vanlancker@hotmail.com>
2020-02-25 16:08:53 +0100

    11

--- scripts/generator.sh
+++ scripts/generator.sh
@@ -3,5 +3,6 @@
 #docker run -it -v /tmp/workspace/:/data ddvlanck/html-page-generator:1.0 bash
 #node html_page_generator.js -f data/repositories/OSLOthema-test/standaardenregister-config.json
 
-
-node /app/html_page_generator.js -f /tmp/workspace/repositories/OSLOthema-test/standaardenregister.json
+mkdir -p /tmp/workspace/html_pages/
+cd /app
+node /app/html_page_generator.js -f /tmp/workspace/repositories/OSLOthema-test/standaardenregister.json -o /tmp/workspace/html_pages
