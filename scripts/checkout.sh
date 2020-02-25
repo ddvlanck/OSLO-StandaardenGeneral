6d3098808c3f328f421df01f6ff8f2d43610b91b
Dwight Van Lancker <dwight.vanlancker@hotmail.com>
2020-02-25 10:09:37 +0100

    Changed scripts

--- scripts/checkout.sh
+++ scripts/checkout.sh
@@ -12,11 +12,24 @@ jq . "$ROOTDIR/commit.json"
 if  [ $? -eq 0 ] ; then
    COMMIT=`jq -r .commit $ROOTDIR/commit.json`
    listofchanges=$(git diff --name-only $COMMIT)
-   echo $listofchanges > changes.txt
+   if [ "$listofchanges" == "config/publication.json" ] ; then
+       git show $COMMIT:config/publication.json > prev
+       jq -s '.[0] - .[1]' config/publication.json prev > $ROOTDIR/changedpublications.json
+       cat $ROOTDIR/changedpublications.json
+       echo "true" > $ROOTDIR/haschangedpublications.json
+       cp ${PUBCONFIG} $ROOTDIR/publications.json.old
+#       echo "false" > $ROOTDIR/haschangedpublications.json
+       cp $ROOTDIR/changedpublications.json ${PUBCONFIG}
+
+   else
+       cp ${PUBCONFIG} $ROOTDIR/changedpublications.json
+       echo "false" > $ROOTDIR/haschangedpublications.json
+       echo "process all publication points";
+   fi
 
 else
-   # no previous commit
-   # assumes full rebuild
+   # No previous commit
+   # Assumes full rebuild of all standards
    echo "No previous commit was made."
    echo "Processing all standards in standaardenregister.json"
    cp "$PUBCONFIG" "$ROOTDIR/changedstandards.json"
