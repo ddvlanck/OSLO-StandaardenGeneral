79ae4a2bd6eab99cb0f7d581585ff60c6bcf18e2
Dwight Van Lancker <dwight.vanlancker@hotmail.com>
2020-02-25 11:56:10 +0100

    Changed config and script

--- scripts/checkout.sh
+++ scripts/checkout.sh
@@ -3,34 +3,49 @@
 PUBCONFIG=$2
 ROOTDIR=$1
 
-
-# determin the last changed files
-mkdir -p $ROOTDIR
-curl -o $ROOTDIR/commit.json https://raw.githubusercontent.com/ddvlanck/OSLO-StandaardenGenerated/master/report/commit.json
+# Determine the last changed files
+mkdir -p "$ROOTDIR"
+curl -o "$ROOTDIR/commit.json" https://raw.githubusercontent.com/ddvlanck/OSLO-StandaardenGenerated/master/commit.json
 sleep 5s
 jq . "$ROOTDIR/commit.json"
-if  [ $? -eq 0 ] ; then
-   COMMIT=`jq -r .commit $ROOTDIR/commit.json`
-   listofchanges=$(git diff --name-only $COMMIT)
-   if [ "$listofchanges" == "config/publication.json" ] ; then
-       git show $COMMIT:config/publication.json > prev
-       jq -s '.[0] - .[1]' config/publication.json prev > $ROOTDIR/changedpublications.json
-       cat $ROOTDIR/changedpublications.json
-       echo "true" > $ROOTDIR/haschangedpublications.json
-       cp ${PUBCONFIG} $ROOTDIR/publications.json.old
-#       echo "false" > $ROOTDIR/haschangedpublications.json
-       cp $ROOTDIR/changedpublications.json ${PUBCONFIG}
-
-   else
-       cp ${PUBCONFIG} $ROOTDIR/changedpublications.json
-       echo "false" > $ROOTDIR/haschangedpublications.json
-       echo "process all publication points";
-   fi
+if [ $? -eq 0 ]; then
+  PREV_COMMIT=$(jq -r .commit "$ROOTDIR/commit.json")
+  changedFiles=$(git diff --name-only "$PREV_COMMIT")
+  if [ "$changedFiles" == "standaardenregister.json" ]; then
+    git show "$COMMIT:standaardenregister.json" >previous_version
+    jq -s '.[0] - .[1]' standaardenregister.json previous_version >"$ROOTDIR/changedstandards.json"
+    cat "$ROOTDIR/changedstandards.json"
+    echo "One or more standards were altered"
+  else
+    cp "${PUBCONFIG}" "$ROOTDIR/changedstandards.json"
+    echo "One or more files were changed, possibly a script, so a full rebuild is necessary"
+  fi
 
 else
-   # No previous commit
-   # Assumes full rebuild of all standards
-   echo "No previous commit was made."
-   echo "Processing all standards in standaardenregister.json"
-   cp "$PUBCONFIG" "$ROOTDIR/changedstandards.json"
+  # No previous commit
+  # Assumes full rebuild of all standards
+  echo "No previous commit was made."
+  echo "Processing all standards in standaardenregister.json"
+  cp "$PUBCONFIG" "$ROOTDIR/changedstandards.json"
+fi
+
+#Process all standards that have been changed or were added
+echo "Start processing the standards"
+if cat ${PUBCONFIG} | jq -e . >/dev/null 2>&1; then
+  # only iterate over those that have a repository
+  for row in $(jq -r '.[] | select(.repository)  | @base64 ' "${PUBCONFIG}"); do
+    _jq() {
+      echo "${row}" | base64 --decode | jq -r "${1}"
+    }
+
+    # Get name of repository to create a directory with the same name
+    REPOSITORY=$(_jq '.repository')
+    THEME_NAME=$(echo "$REPOSITORY" | cut -d '/' -f 5)
+
+    mkdir -p $ROOTDIR/$THEME_NAME
+    git clone $REPOSITORY $ROOTDIR/$THEME_NAME
+
+  done
+else
+  echo "Problem processing following file: ${PUBCONFIG}"
 fi
