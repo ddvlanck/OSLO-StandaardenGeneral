9ec82b16c4af3bd16b34e14205938ec4795e87a4
Dwight Van Lancker <dwight.vanlancker@hotmail.com>
2020-02-26 11:49:07 +0100

    16

--- scripts/generator.sh
+++ scripts/generator.sh
@@ -9,11 +9,6 @@ cd /app
 for REPO in $(find "$REPODIR" -maxdepth 1 -mindepth 1 -type d)
 do
   THEME_NAME=$(echo "$REPO" | cut -d "/" -f 5 | cut -d "-" -f 2)
-  node html_page_generator.js -f "$REPO"/standaardenregister.json -o "$RESULTDIR"/"$THEME_NAME"
+  node html_page_generator.js -f "$REPO"/standaardenregister.json -o "$RESULTDIR"/"$THEME_NAME"-index.html
 done
 
-#mkdir -p /tmp/workspace/html_pages/
-#cd /app
-#node html_page_generator.js -f /tmp/workspace/repositories/OSLOthema-test/standaardenregister.json -o /tmp/workspace/html_pages
-
-#TODO generator aanpassen zodat volledige naam van het bestand moet meegegeven worden!
