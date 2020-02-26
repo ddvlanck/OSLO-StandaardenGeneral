54a6df70e02b32ef1a07b2431205aa9e0b6b25ca
Dwight Van Lancker <dwight.vanlancker@hotmail.com>
2020-02-26 11:45:14 +0100

    15

--- scripts/generator.sh
+++ scripts/generator.sh
@@ -4,11 +4,12 @@ RESULTDIR=/tmp/workspace/html_pages
 REPODIR=/tmp/workspace/repositories
 
 mkdir -p "$RESULTDIR"
+cd /app
 
-for repo in $(find "$REPODIR" -maxdepth 1 -mindepth 1 -type d)
+for REPO in $(find "$REPODIR" -maxdepth 1 -mindepth 1 -type d)
 do
-  THEME_NAME=$(echo $repo | cut -d "/" -f 4 | cut -d "-" -f 2)
-  echo $THEME_NAME
+  THEME_NAME=$(echo "$REPO" | cut -d "/" -f 5 | cut -d "-" -f 2)
+  node html_page_generator.js -f "$REPO"/standaardenregister.json -o "$RESULTDIR"/"$THEME_NAME"
 done
 
 #mkdir -p /tmp/workspace/html_pages/
