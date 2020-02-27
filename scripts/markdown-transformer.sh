948d55601073f83617b507fb6dea2a8d96d3f402
Dwight Van Lancker <dwight.vanlancker@hotmail.com>
2020-02-27 14:29:12 +0100

    Updated transform script

--- scripts/markdown-transformer.sh
+++ scripts/markdown-transformer.sh
@@ -8,5 +8,7 @@ for REPO in $(find "$REPODIR" -maxdepth 1 -mindepth 1 -type d)
 do
   THEME_NAME=$(echo "$REPO" | cut -d "/" -f 5 | cut -d "-" -f 2)
   cd $REPO
-  echo README.md
+  if test -f "README.md" ; then
+    node /app/index.js -f README.md -o $ROOTDIR/descriptions/$THEME_NAME-description.html
+  fi  
 done
