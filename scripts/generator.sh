83ce1a0c24569e64f62a13dd72fe58a1f57d8e01
Dwight Van Lancker <dwight.vanlancker@hotmail.com>
2020-02-28 11:15:22 +0100

    update generator script to put pages in correct folder according to their status

--- scripts/generator.sh
+++ scripts/generator.sh
@@ -4,6 +4,10 @@ ROOTDIR=/tmp/workspace
 RESULTDIR=/tmp/workspace/html_pages
 REPODIR=/tmp/workspace/repositories
 
+mkdir -p "$RESULTDIR/erkende-standaard"
+mkdir -p "$RESULTDIR/standaard-in-ontwikkeling"
+mkdir -p "$RESULTDIR/kandidaat-standaard"
+
 mkdir -p "$RESULTDIR"
 cd /app
 
@@ -11,9 +15,11 @@ for REPO in $(find "$REPODIR" -maxdepth 1 -mindepth 1 -type d)
 do
   THEME_NAME=$(echo "$REPO" | cut -d "/" -f 5 | cut -d "-" -f 2,3)
   DESCRIPTION="$ROOTDIR/descriptions/$THEME_NAME-description.html"
+  STATUS=$(echo "$ROOTDIR/status.txt" | grep "$THEME_NAME" | cut -d ":" -f 2)
+
   if test -f "$DESCRIPTION" ; then
     echo "A description was provided for the $THEME_NAME repository"
-    node html_page_generator.js -f "$REPO/config.json" -o "$RESULTDIR"/"$THEME_NAME"-index.html -t "$ROOTDIR/descriptions/$THEME_NAME-description.html"
+    node html_page_generator.js -f "$REPO/config.json" -o "$RESULTDIR/$STATUS/$THEME_NAME-index.html" -t "$ROOTDIR/descriptions/$THEME_NAME-description.html"
   else
     echo "No description was provided for the $THEME_NAME repository"
     node html_page_generator.js -f "$REPO/config.json" -o "$RESULTDIR/$THEME_NAME-index.html"
