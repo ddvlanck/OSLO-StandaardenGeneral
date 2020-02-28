dbd2559c11d6d07574c6c829b19a191d3f506f42
Dwight Van Lancker <dwight.vanlancker@hotmail.com>
2020-02-28 11:22:50 +0100

    Typo

--- scripts/generator.sh
+++ scripts/generator.sh
@@ -15,14 +15,14 @@ for REPO in $(find "$REPODIR" -maxdepth 1 -mindepth 1 -type d)
 do
   THEME_NAME=$(echo "$REPO" | cut -d "/" -f 5 | cut -d "-" -f 2,3)
   DESCRIPTION="$ROOTDIR/descriptions/$THEME_NAME-description.html"
-  STATUS=$(echo "$ROOTDIR/status.txt" | grep "$THEME_NAME" | cut -d ":" -f 2)
+  STATUS=$(cat "$ROOTDIR/status.txt" | grep "$THEME_NAME" | cut -d ":" -f 2)
 
   if test -f "$DESCRIPTION" ; then
     echo "A description was provided for the $THEME_NAME repository"
     node html_page_generator.js -f "$REPO/config.json" -o "$RESULTDIR/$STATUS/$THEME_NAME-index.html" -t "$ROOTDIR/descriptions/$THEME_NAME-description.html"
   else
     echo "No description was provided for the $THEME_NAME repository"
-    node html_page_generator.js -f "$REPO/config.json" -o "$RESULTDIR/$THEME_NAME-index.html"
+    node html_page_generator.js -f "$REPO/config.json" -o "$RESULTDIR/$STATUS/$THEME_NAME-index.html"
   fi
 done
 
