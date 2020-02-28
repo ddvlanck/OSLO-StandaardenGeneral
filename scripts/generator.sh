d50bbef4b9a840bbc3637b729623a673d9e842fc
Dwight Van Lancker <dwight.vanlancker@hotmail.com>
2020-02-28 09:00:22 +0100

    Added some logging

--- scripts/generator.sh
+++ scripts/generator.sh
@@ -12,8 +12,10 @@ do
   THEME_NAME=$(echo "$REPO" | cut -d "/" -f 5 | cut -d "-" -f 2)
   DESCRIPTION="$ROOTDIR/descriptions/$THEME_NAME-description.html"
   if test -f "$DESCRIPTION" ; then
+    echo "A description was provided for the $THEME_NAME repository"
     node html_page_generator.js -f "$REPO"/config.json -o "$RESULTDIR"/"$THEME_NAME"-index.html -d "$ROOTDIR/descriptions/$THEME_NAME-description.html"
   else
+    echo "No description was provided for the $THEME_NAME repository"
     node html_page_generator.js -f "$REPO"/config.json -o "$RESULTDIR"/"$THEME_NAME"-index.html
   fi
 done
