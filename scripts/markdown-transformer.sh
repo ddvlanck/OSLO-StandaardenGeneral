2afa870f6e8c95262478debf61f418e7f6247598
Dwight Van Lancker <dwight.vanlancker@hotmail.com>
2020-02-28 10:12:17 +0100

    Updated theme name in transform script

--- scripts/markdown-transformer.sh
+++ scripts/markdown-transformer.sh
@@ -6,7 +6,7 @@ mkdir -p "$ROOTDIR/descriptions"
 
 for REPO in $(find "$REPODIR" -maxdepth 1 -mindepth 1 -type d)
 do
-  THEME_NAME=$(echo "$REPO" | cut -d "/" -f 5 | cut -d "-" -f 2)
+  THEME_NAME=$(echo "$REPO" | cut -d "/" -f 5 | cut -d "-" -f 2,3)
   cd $REPO
   if test -f "README.md" ; then
     node /app/index.js -f README.md -o "$ROOTDIR/descriptions/$THEME_NAME-description.html"
