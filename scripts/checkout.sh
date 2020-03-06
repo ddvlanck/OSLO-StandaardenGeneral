a9e50fab030cbf56d8fc68b2dd8cbe8b2033ea2f
Dwight Van Lancker <dwight.vanlancker@hotmail.com>
2020-03-06 12:01:46 +0100

    Added extended config file for repo's

--- scripts/checkout.sh
+++ scripts/checkout.sh
@@ -63,9 +63,8 @@ if cat "$ROOTDIR/changedstandards.json" | jq -e . >/dev/null 2>&1; then
 
     # We extend the the configuration file for a specific file in a theme with the baseURL
     BASE_URL="https://github.com/ddvlanck/$THEME_NAME/raw/standaardenregister"
-    jq --arg BASE_URL "$BASE_URL" '. |= . + {"baseURL" : $BASE_URL}' "$CONFIG"
+    jq --arg BASE_URL "$BASE_URL" '. |= . + {"baseURL" : $BASE_URL}' "$CONFIG" > "$CONFIG-extended.json"
 
-    cat "$CONFIG"
 
   done
 else
--- scripts/generator.sh
+++ scripts/generator.sh
@@ -24,10 +24,10 @@ do
   cd /app
   if test -f "$DESCRIPTION" ; then
     echo "A description was provided for repository: $REPO_NAME"
-    node html_page_generator.js -f "$FULL_REPO_PATH/$CONFIG" -o "$RESULTDIR/$STATUS/$CONFIG_NAME-index.html" -t "$DESCRIPTION"
+    node html_page_generator.js -f "$FULL_REPO_PATH/$CONFIG-extended.json" -o "$RESULTDIR/$STATUS/$CONFIG_NAME-index.html" -t "$DESCRIPTION"
   else
     echo "No description was provided for repository: $REPO_NAME"
-    node html_page_generator.js -f "$FULL_REPO_PATH/$CONFIG" -o "$RESULTDIR/$STATUS/$CONFIG_NAME-index.html"
+    node html_page_generator.js -f "$FULL_REPO_PATH/$CONFIG-extended.json" -o "$RESULTDIR/$STATUS/$CONFIG_NAME-index.html"
   fi
 
 done < "$ROOTDIR/tmp-register.txt"
